aeed7e3c1c652f5ecc9a1ae3a9e584343c2a52e1
zenith16f <fguzmanc2100@alumno.ipn.mx>
2023-02-20 23:28:16 -0600

    Practice ended

--- 03Registro/JS/validacion.js
+++ 03Registro/JS/validacion.js
@@ -13,72 +13,65 @@ CONST es un valor constante
 
 function validar(formulario) {
   if (formulario.nombre.value.length <= 3) {
-    alert("Ingresar mas de 3 caracteres en el campo nombre");
+    alert("Favor de ingresar más de 3 caracteres en el campo nombre");
     formulario.nombre.focus();
     return false;
   }
-
-  var checarABC =
-    "qwertyuiopasdfghjklñzxcvbnm " + "QWERTYUIOPASDFGHJKLÑZXCVBNM ";
+  var checarABC = "qazwsxedcrfvtgbyhnujmikolpñ" + "QAZWSXEDCRFVTGBYHNUJMIKOLPÑ";
 
   var cadenaNombre = formulario.nombre.value;
+
   //alert(cadenaNombre);
 
-  var todoEsValido = true;
+  var todoesvalido = true;
 
   for (var i = 0; i < cadenaNombre.length; i++) {
-    var caracteres = cadenaNombre.charAT(i);
+    var caracteres = cadenaNombre.charAt(i);
     for (var j = 0; j < checarABC.length; j++) {
       if (caracteres == checarABC.charAt(j)) {
         break;
       }
     }
     if (j == checarABC.length) {
-      todoEsValido = false;
+      todoesvalido = false;
       break;
     }
   }
-
-  if (!todoEsValido) {
-    alert("Ingresa solo letras en el nombre");
+  if (!todoesvalido) {
+    alert("Ingresa solo letras en el campo nombre");
     formulario.nombre.focus();
     return false;
   }
 
-  //Edad
-
   var edad = parseInt(formulario.edad.value);
-
   alert(edad);
-
   if (edad < 0 || edad >= 99) {
-    alert("Ingresa una edad valida");
+    alert("Ingresa una edad valida entre 01 y 99 años");
     formulario.edad.focus();
     return false;
   }
-
   var checarABC = "0123456789";
 
-  var cadenaNombre = formulario.nombre.value;
+  var cadenaNombre = formulario.edad.value;
+
   //alert(cadenaNombre);
 
-  var todoEsValido = true;
+  var todoesvalido = true;
 
   for (var i = 0; i < cadenaNombre.length; i++) {
-    var caracteres = cadenaNombre.charAT(i);
+    var caracteres = cadenaNombre.charAt(i);
     for (var j = 0; j < checarABC.length; j++) {
       if (caracteres == checarABC.charAt(j)) {
         break;
       }
     }
     if (j == checarABC.length) {
-      todoEsValido = false;
+      todoesvalido = false;
       break;
     }
   }
-
-  if (!todoEsValido) {
-    alert("Ingresa solo numeros en el campo de edad");
+  if (!todoesvalido) {
+    alert("Ingresa solo numeros en el campo edad");
     formulario.edad.focus();
     return false;
   }
