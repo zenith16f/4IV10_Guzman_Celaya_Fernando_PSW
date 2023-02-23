e40bcabde3f9b456c5c6c057665a197730121a6c
zenith16f <fguzmanc2100@alumno.ipn.mx>
2023-02-23 16:49:37 -0600

    Some changes

--- Ejercicios/JS/functionSeis-Diez.js
+++ Ejercicios/JS/functionSeis-Diez.js
@@ -1,3 +1,4 @@
+// Sixth answer
 function sixth() {
   let ingresoNacimiento = document.getElementById("fecha").value;
   let nacimientoIngresado = new Date(ingresoNacimiento);
@@ -26,6 +27,35 @@ function sixth() {
 
     alert("Tu edad es de: " + edadAño + " años");
   }
-  //   alert(ingresoNacimiento);
-  //   alert(fechaActual);
 }
+
+// Seventh answer
+function seventh() {}
+
+// Eighth answer
+function eighth() {
+  let numberOne = document.getElementById("numberOne").value;
+  let numberTwo = document.getElementById("numberTwo").value;
+  let numberThree = document.getElementById("numberThree").value;
+  let mayor;
+  let lector = /\D/;
+
+  if (
+    lector.test(numberOne) ||
+    lector.test(numberTwo) ||
+    lector.test(numberThree)
+  ) {
+    alert("Ingresa datos validos");
+    return false;
+  } else {
+    mayor = Math.max(numberOne, numberTwo, numberThree);
+    console.log(mayor);
+  }
+  //   console.log(numberOne + "" + numberTwo + "" + numberThree);
+}
+
+// Ninth answer
+function ninth() {}
+
+// Tenth answer
+function tenth() {}
--- Ejercicios/JS/functionUno-Cinco.js
+++ Ejercicios/JS/functionUno-Cinco.js
@@ -7,42 +7,42 @@ function first() {
   let ganancia;
   let totalCambiante;
   let totalFinal;
-  let fobidden = /([0-9]*)\./;
+  let fobidden = /\D\./;
 
   //Validacion de datos
-  if (fobidden.test(capitalIngresado)) {
-    alert("Ingresa un capital valido");
+  if (fobidden.test(capitalIngresado) || fobidden.test(tiempoIngresado)) {
+    alert("Ingresa un dato valido");
     return false;
-  }
-
-  if (fobidden.test(tiempoIngresado)) {
-    alert("Ingresa un tiempo valido");
-  }
-
-  //Proceso
-  if (tiempoIngresado > 0 && tiempoIngresado <= 36) {
-    if (capitalIngresado > 0) {
-      for (let i = 0; i < tiempoIngresado; i++) {
-        ganancia = capitalIngresado * 0.08;
-        totalCambiante = capitalIngresado + ganancia;
-        capitalIngresado = totalCambiante;
+  } else {
+    //Proceso
+    if (tiempoIngresado > 0 && tiempoIngresado <= 36) {
+      if (capitalIngresado > 0) {
+        for (let i = 0; i < tiempoIngresado; i++) {
+          ganancia = capitalIngresado * 0.08;
+          totalCambiante = capitalIngresado + ganancia;
+          capitalIngresado = totalCambiante;
+        }
+        totalFinal = totalCambiante;
+        alert("$" + totalFinal.toFixed(2));
+      } else {
+        alert("Ingresa una cantidad de capital valida");
+        return false;
       }
-      totalFinal = totalCambiante;
-      alert("$" + totalFinal.toFixed(2));
     } else {
-      alert("Ingresa una cantidad de capital valida");
+      alert("Los meses ingresados son invalidos");
       return false;
     }
-  } else {
-    alert("Los meses ingresados son invalidos");
-    return false;
   }
 }
 
 // Second answer
+function second() {}
 
 // Third answer
+function third() {}
 
 // Fourth answer
+function fourth() {}
 
 // Fifth answer
+function fifth() {}
