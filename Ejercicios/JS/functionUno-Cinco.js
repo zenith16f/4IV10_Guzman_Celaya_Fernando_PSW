6c1a7906d6f5305155f1913401edf192e506a0ae
zenith16f <fguzmanc2100@alumno.ipn.mx>
2023-02-23 02:59:02 -0600

    Starting answering the exercises

---
+++ Ejercicios/JS/functionUno-Cinco.js
@@ -0,0 +1,19 @@
+// First answer
+
+function first() {
+  let capitalIngresado;
+  let tiempoIngresado;
+  let capitalObtenido;
+  let ganancia;
+  let ingresado = document.getElementById("money");
+  console.log(ingresado.value * 2);
+  return false;
+}
+
+// Second answer
+
+// Third answer
+
+// Fourth answer
+
+// Fifth answer
