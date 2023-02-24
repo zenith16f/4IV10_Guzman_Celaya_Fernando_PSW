8015e38384747683123d0ade52b8d520cb70a0c0
zenith16f <fguzmanc2100@alumno.ipn.mx>
2023-02-24 03:27:17 -0600

    Sixth to tenth exercises ended

--- Ejercicios/JS/functionSeis-Diez.js
+++ Ejercicios/JS/functionSeis-Diez.js
@@ -30,7 +30,61 @@ function sixth() {
 }
 
 // Seventh answer
-function seventh() {}
+function seventh() {
+  let numeroUno = document.getElementById("numeroUno").value;
+  let numeroDos = document.getElementById("numeroDos").value;
+  let resultado;
+  let lector = /\D/;
+
+  if (lector.test(numeroUno) || lector.test(numeroDos)) {
+    alert("Ingresa datos validos");
+    return false;
+  } else {
+    numeroUno = parseFloat(numeroUno);
+    numeroDos = parseFloat(numeroDos);
+    if (numeroUno < 1000 || numeroDos < 1000) {
+      if (numeroUno > numeroDos) {
+        resultado = Math.pow(numeroUno, numeroDos);
+        alert(
+          "El numero mayor es: " +
+            numeroUno +
+            " entonces el resultado de la operacion es: " +
+            resultado
+        );
+      } else if (numeroDos > numeroUno) {
+        resultado = numeroDos / numeroUno;
+        alert(
+          "El numero mayor es: " +
+            numeroDos +
+            " entonces el resultado de la operacion es: " +
+            resultado
+        );
+      } else if (numeroDos == numeroUno) {
+        resultado = numeroUno * numeroDos;
+        alert(
+          "Los numeros son iguales entonces el resultado de la operacion es: " +
+            resultado
+        );
+      }
+    } else {
+      alert("Ingresa numeros menores a 1000");
+      return false;
+    }
+  }
+}
+
+function seventhI(e) {
+  let teclado = document.all ? e.keyCode : e.which;
+  if (teclado == 8) return true;
+  let patron = /[0-9\d .]/;
+  let codigo = String.fromCharCode(teclado);
+  return patron.test(codigo);
+}
+
+function seventhV() {
+  document.getElementById("numeroUno").value = "";
+  document.getElementById("numeroDos").value = "";
+}
 
 // Eighth answer
 function eighth() {
@@ -51,7 +105,6 @@ function eighth() {
     mayor = Math.max(numberOne, numberTwo, numberThree);
     alert("El numero mayor es: " + mayor);
   }
-  //   console.log(numberOne + "" + numberTwo + "" + numberThree);
 }
 
 // Ninth answer
@@ -83,27 +136,85 @@ function ninth() {
       pagoExtra = false;
     }
 
-    if (pagoExtra == true) {
-      horasSobrantes = horasTrabajadas - 40;
-      if (horasSobrantes > 8) {
-        horasTriple = horasSobrantes - 8;
-        horasDoble = horasSobrantes - horasTriple;
-        pagoDoble = horasDoble * (salario * 2);
-        pagoTriple = horasTriple * (salario * 3);
-        pago = 40 * salario;
-        pagoTotal = pago + pagoDoble + pagoTriple;
+    if (
+      salario > 0 &&
+      salario < 999999 &&
+      horasTrabajadas > 0 &&
+      horasTrabajadas < 100000
+    ) {
+      if (pagoExtra == true) {
+        horasSobrantes = horasTrabajadas - 40;
+        if (horasSobrantes > 8) {
+          horasTriple = horasSobrantes - 8;
+          horasDoble = horasSobrantes - horasTriple;
+          pagoDoble = horasDoble * (salario * 2);
+          pagoTriple = horasTriple * (salario * 3);
+          pago = 40 * salario;
+          pagoTotal = pago + pagoDoble + pagoTriple;
+        } else {
+          horasDoble = horasSobrantes;
+          pagoDoble = horasDoble * (salario * 2);
+          pago = 40 * salario;
+          pagoTotal = pago + pagoDoble;
+        }
       } else {
-        horasDoble = horasSobrantes;
-        pagoDoble = horasDoble * (salario * 2);
-        pago = 40 * salario;
-        pagoTotal = pago + pagoDoble;
+        pagoTotal = horasTrabajadas * salario;
       }
+      alert("Tu pago de todo es: $" + pagoTotal);
     } else {
-      pagoTotal = horasTrabajadas * salario;
+      alert("Ingresa datos validos");
+      return false;
     }
-    alert(pagoTotal);
   }
 }
 
 // Tenth answer
-function tenth() {}
+function tenth() {
+  let salario = parseInt(document.getElementById("lastment").value);
+  let antiguedad = parseInt(document.getElementById("years").value);
+  let utilidad;
+  let pago;
+  let lector = /\D/;
+
+  if (lector.test(salario) || lector.test(antiguedad)) {
+    alert("Ingresa datos validos");
+    return false;
+  } else {
+    if (antiguedad > 0 && antiguedad < 70) {
+      if (salario > 0 && salario < 999999) {
+        if (antiguedad < 1) {
+          utilidad = 0.05;
+        } else if (antiguedad >= 1 && antiguedad <= 2) {
+          utilidad = 0.07;
+        } else if (antiguedad >= 2 && antiguedad <= 5) {
+          utilidad = 0.1;
+        } else if (antiguedad >= 5 && antiguedad <= 10) {
+          utilidad = 0.15;
+        } else if (antiguedad > 10) {
+          utilidad = 0.2;
+        }
+        pago = salario * utilidad;
+        alert("Tu utilidad es: $" + pago);
+      } else {
+        alert("Ingresa un salario valido");
+        return false;
+      }
+    } else {
+      alert("Ingresa unos años validos");
+      return false;
+    }
+  }
+}
+
+function tenthI(e) {
+  let teclado = document.all ? e.keyCode : e.which;
+  if (teclado == 8) return true;
+  let patron = /[0-9\d .]/;
+  let codigo = String.fromCharCode(teclado);
+  return patron.test(codigo);
+}
+
+function tenthV() {
+  document.getElementById("lastment").value = "";
+  document.getElementById("years").value = "";
+}
--- Ejercicios/JS/functionUno-Cinco.js
+++ Ejercicios/JS/functionUno-Cinco.js
@@ -1,29 +1,35 @@
 // First answer
-
 function first() {
   //Varaibles
-  let capitalIngresado = parseFloat(document.getElementById("money").value);
-  let tiempoIngresado = parseInt(document.getElementById("time").value);
+  let capitalIngresado = document.getElementById("money").value;
+  let tiempoIngresado = document.getElementById("time").value;
   let ganancia;
   let totalCambiante;
   let totalFinal;
-  let fobidden = /\D\./;
 
-  //Validacion de datos
-  if (fobidden.test(capitalIngresado) || fobidden.test(tiempoIngresado)) {
-    alert("Ingresa un dato valido");
+  //Proceso
+  let lector = /\D/;
+
+  if (lector.test(capitalIngresado) || lector.test(tiempoIngresado)) {
+    alert("Ingresa datos validos");
     return false;
   } else {
-    //Proceso
+    capitalIngresado = parseFloat(capitalIngresado);
+    tiempoIngresado = parseFloat(tiempoIngresado);
     if (tiempoIngresado > 0 && tiempoIngresado <= 36) {
-      if (capitalIngresado > 0) {
+      if (capitalIngresado > 0 && capitalIngresado <= 999999) {
         for (let i = 0; i < tiempoIngresado; i++) {
           ganancia = capitalIngresado * 0.08;
           totalCambiante = capitalIngresado + ganancia;
           capitalIngresado = totalCambiante;
         }
         totalFinal = totalCambiante;
-        alert("$" + totalFinal.toFixed(2));
+        alert(
+          "Tu inversion en " +
+            tiempoIngresado +
+            " meses genera: $" +
+            totalFinal.toFixed(2)
+        );
       } else {
         alert("Ingresa una cantidad de capital valida");
         return false;
@@ -33,6 +39,19 @@ function first() {
       return false;
     }
   }
+}
+
+function firstI(e) {
+  let teclado = document.all ? e.keyCode : e.which;
+  if (teclado == 8) return true;
+  let patron = /[0-9\d .]/;
+  let codigo = String.fromCharCode(teclado);
+  return patron.test(codigo);
+}
+
+function vaciar() {
+  document.getElementById("money").value = "";
+  document.getElementById("time").value = "";
 }
 
 // Second answer
